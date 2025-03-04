7cb37ea115d895ec8129eb132d74d35a84937b0f
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2025-03-03 18:38:42 -0600

    added fix for cleanDB.ts file

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -2,6 +2,9 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
+
+  if (models[modelName].db.db === undefined) throw new Error("Model is undefined");
+  
   try {
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
