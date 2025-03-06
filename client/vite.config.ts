0d2869268650b3ed51f44a6f6b548d436ec2d281
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2025-03-06 17:46:50 -0600

    changed vite.vonfig file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    // host: '0.0.0.0',
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3001',
